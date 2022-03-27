b032d9d52eb8696a7923ca3f4a1811419862316f
Thomas Perry <incinx@gmail.com>
2022-03-27 15:54:24 -0700

    Initial system implementation

---
+++ module/helpers/config.mjs
@@ -0,0 +1,29 @@
+export const FARHOME = {};
+
+// Define constants here, such as:
+FARHOME.foobar = {
+  'bas': 'FARHOME.bas',
+  'bar': 'FARHOME.bar'
+};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ FARHOME.abilities = {
+  "str": "FARHOME.AbilityStr",
+  "dex": "FARHOME.AbilityDex",
+  "con": "FARHOME.AbilityCon",
+  "int": "FARHOME.AbilityInt",
+  "wis": "FARHOME.AbilityWis",
+  "cha": "FARHOME.AbilityCha"
+};
+
+FARHOME.abilityAbbreviations = {
+  "str": "FARHOME.AbilityStrAbbr",
+  "dex": "FARHOME.AbilityDexAbbr",
+  "con": "FARHOME.AbilityConAbbr",
+  "int": "FARHOME.AbilityIntAbbr",
+  "wis": "FARHOME.AbilityWisAbbr",
+  "cha": "FARHOME.AbilityChaAbbr"
+};
