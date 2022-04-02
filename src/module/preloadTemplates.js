bff5cb24c38887aa15e8aa2d8ae8f6addc85ff61
Thomas Perry <incinx@gmail.com>
2022-04-02 00:21:20 -0700

    Using partials and loops for things, huzzah!

--- src/module/preloadTemplates.js
+++ src/module/preloadTemplates.js
@@ -1,5 +1,6 @@
 export async function preloadTemplates() {
   const templatePaths = [
+    'systems/farhome/templates/partials/attribute-partial.hbs',
     'systems/farhome/templates/partials/max-resource-partial.hbs',
     'systems/farhome/templates/partials/resource-partial.hbs',
   ];
