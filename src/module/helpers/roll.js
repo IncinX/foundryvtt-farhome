039b8e4d6073477aace8610b876b88e343b5fc47
Thomas Perry <incinx@gmail.com>
2022-04-03 01:28:44 -0700

    Fix extra div and remove todo.

--- src/module/helpers/roll.js
+++ src/module/helpers/roll.js
@@ -2,9 +2,6 @@
 import { clamp } from './math';
 
 export function proficiencyRollFormula(proficiency, attribute) {
-  // TODO There is a bug with attribute 6, proficiency 1.  There should be 1 yellow, 4 green, 1 white. But it's not.
-  // TODO Same bug with attribute 6, profiency 5.  There should be 5 yellow, 1 white.  But it's not.
-
   const standardMaxDice = 5;
   let maxDice = Math.max(standardMaxDice, attribute);
   let maxSuperior = clamp(attribute, 0, standardMaxDice);
