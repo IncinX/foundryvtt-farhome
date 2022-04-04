e2130b0f8db2cdd183f475ab47cedf0ad0538d0c
Thomas Perry <incinx@gmail.com>
2022-04-04 14:10:37 -0700

    Add TODO's for the config

--- src/module/helpers/config.js
+++ src/module/helpers/config.js
@@ -1,1 +1,5 @@
 export const FARHOME = {};
+
+// TODO Add stuff here for spell school drop down box (from training video)
+// TODO Add stuff here from range selectors for spells
+// TODO Add stuff here for range selectors for weapons
