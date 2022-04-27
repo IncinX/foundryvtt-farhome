937d1435bce81ad45486c491f6dd55f1227dbb2d
Thomas Perry <incinx@gmail.com>
2022-04-27 00:08:53 -0700

    Add partials for character sheets to reduce code duplication.

--- src/module/preload-templates.js
+++ src/module/preload-templates.js
@@ -1,5 +1,10 @@
 export async function preloadTemplates() {
   const templatePaths = [
+    'systems/farhome/templates/partials/actor/attributes-partial.hbs',
+    'systems/farhome/templates/partials/actor/feats-partial.hbs',
+    'systems/farhome/templates/partials/actor/maneuvers-partial.hbs',
+    'systems/farhome/templates/partials/actor/spells-partial.hbs',
+    'systems/farhome/templates/partials/actor/inventory-partial.hbs',
     'systems/farhome/templates/partials/attribute-partial.hbs',
     'systems/farhome/templates/partials/max-resource-partial.hbs',
     'systems/farhome/templates/partials/resource-partial.hbs',
