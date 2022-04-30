c180293d981c0883bf849e6bea6927753b4b34f5
Thomas Perry <incinx@gmail.com>
2022-04-30 14:10:56 -0700

    Added custom roll templates for weapons, spells, maneuvers and armor.

--- src/module/sheets/actor-sheet.js
+++ src/module/sheets/actor-sheet.js
@@ -1,7 +1,14 @@
 import { sendActorMessage } from '../helpers/chat.js';
 import { onManageActiveEffect, prepareActiveEffectCategories } from '../helpers/effects.js';
 import { localizeObject } from '../helpers/localization.js';
-import { _getDefaultRollTemplate } from '../helpers/roll-templates.js';
+import {
+  getStrongestKey,
+  getDefaultRollTemplate,
+  getWeaponRollTemplate,
+  getArmorRollTemplate,
+  getSpellRollTemplate,
+  getManeuverRollTemplate,
+} from '../helpers/roll-templates.js';
 
 // TODO Add Poison/Hex icons later
 
@@ -233,7 +240,7 @@ export default class FarhomeActorSheet extends ActorSheet {
     const type = header.dataset.type;
 
     // Grab any data associated with this control.
-    const data = duplicate(header.dataset);
+    const controlData = duplicate(header.dataset);
 
     // Initialize a default name.
     const name = `New ${type.capitalize()}`;
@@ -245,17 +252,56 @@ export default class FarhomeActorSheet extends ActorSheet {
       data: {},
     };
 
+    // Populate the spell item
     if (type === 'spell') {
       itemData.data.spellLevel = {
-        value: parseInt(data.spellLevel),
+        value: parseInt(controlData.spellLevel),
       };
     }
 
-    // TODO When a new item of a type is created, it should fill the rollTemplate field with an appropriate template for it's type.
-    //      This roll template should vary depending on type.
-    itemData.data.rollTemplate = {
-      value: _getDefaultRollTemplate(),
-    };
+    // Setup a custom template depending on the item type.
+    const actorData = this.actor.data.data;
+
+    if (type === 'armor') {
+      itemData.data.rollTemplate = {
+        value: getArmorRollTemplate(),
+      };
+    } else if (type === 'weapon' || type === 'maneuver') {
+      const relevantWeaponAttributes = {
+        str: actorData.attributes.str,
+        dex: actorData.attributes.dex,
+      };
+
+      const strongestWeaponProficiency = getStrongestKey(actorData.proficiencies.weapons);
+      const strongestAttribute = getStrongestKey(relevantWeaponAttributes);
+
+      if (type === 'weapon') {
+        itemData.data.rollTemplate = {
+          value: getWeaponRollTemplate(`a.${strongestWeaponProficiency}`, `a.${strongestAttribute}`),
+        };
+      } else {
+        itemData.data.rollTemplate = {
+          value: getManeuverRollTemplate(`a.${strongestWeaponProficiency}`, `a.${strongestAttribute}`),
+        };
+      }
+    } else if (type === 'spell') {
+      const relevantSpellAttributes = {
+        sta: actorData.attributes.sta,
+        int: actorData.attributes.int,
+        will: actorData.attributes.will,
+        cha: actorData.attributes.cha,
+      };
+
+      const strongestSpellProficiency = getStrongestKey(actorData.proficiencies.spells);
+      const strongestAttribute = getStrongestKey(relevantSpellAttributes);
+      itemData.data.rollTemplate = {
+        value: getSpellRollTemplate(`a.${strongestSpellProficiency}`, `a.${strongestAttribute}`),
+      };
+    } else {
+      itemData.data.rollTemplate = {
+        value: getDefaultRollTemplate(),
+      };
+    }
 
     // Finally, create the item!
     return await Item.create(itemData, { parent: this.actor });
