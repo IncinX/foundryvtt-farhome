93800291e01442e4ab55a6d7896efe9277dcfc12
IncinX <incinx@gmail.com>
2022-09-02 17:48:42 +0000

    Update rolling rules according to https://github.com/scrabbletank/farhome-rules/commit/3315fc480e9140112bef6d93efd49c65171ca9c7

--- src/module/helpers/roll.js
+++ src/module/helpers/roll.js
@@ -1,13 +1,24 @@
 import { clamp } from './math';
 
+// #todo Need to change the roll formula for 6+ attr based on this PR (https://github.com/scrabbletank/farhome-rules/commit/3315fc480e9140112bef6d93efd49c65171ca9c7)
+
 export function proficiencyRollFormula(proficiency, attribute) {
   const standardMaxDice = 5;
+
+  // Figure out the max dice increase based on the attribute
   let maxDice = Math.max(standardMaxDice, attribute);
+
+  // Calculate the maximum number of superior dice that can be converted
   let maxSuperior = clamp(attribute, 0, standardMaxDice);
+
+  // Determine the number of superior dice based on the proficiency
   let superiorDice = Math.min(proficiency, maxSuperior);
-  let extraEnhanced = maxDice > standardMaxDice ? Math.max(proficiency - standardMaxDice, 0) : 0;
-  let enhancedDice = clamp(attribute, 0, standardMaxDice) - superiorDice + extraEnhanced;
-  let normalDice = maxDice - (enhancedDice + superiorDice);
+
+  // Determine the number of enhanced dice based on the attribute and subtract those that have been converted to superior dice.
+  let enhancedDice = Math.max(attribute, 0) - superiorDice;
+
+  // Fill the remainder of the deice with normal dice up to the standard maximum and a minimum of 0.
+  let normalDice = Math.max(standardMaxDice - (enhancedDice + superiorDice), 0);
 
   // Handle negative attributes
   let badDice = attribute < 0 ? Math.min(Math.abs(attribute), standardMaxDice) : 0;
