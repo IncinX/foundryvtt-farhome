49b6e3f0a556cf32cba19faeed18fe047e26a1af
IncinX <incinx@gmail.com>
2022-09-05 21:50:03 +0000

    Fix all lint errors (excluding lint warnings)

--- src/module/importers/farhome-rules.test.js
+++ src/module/importers/farhome-rules.test.js
@@ -2,11 +2,11 @@ import 'isomorphic-fetch';
 import { parseRules } from './farhome-rules';
 
 test('Parse Farhome Original Rules', async () => {
-  const rulesUri = 'https://raw.githubusercontent.com/scrabbletank/farhome-rules/2aebacdcb0124c2cc5001a0d031e12eb5025cf2f/rulesdoc.md';
+  const rulesUri =
+    'https://raw.githubusercontent.com/scrabbletank/farhome-rules/2aebacdcb0124c2cc5001a0d031e12eb5025cf2f/rulesdoc.md';
   const data = await fetch(rulesUri);
   const markdownText = await data.text();
   parseRules(markdownText);
 });
 
-test('Parse Farhome Eberron Rules', async () => {
-});
+test('Parse Farhome Eberron Rules', async () => {});
