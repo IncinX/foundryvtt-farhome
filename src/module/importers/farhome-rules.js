88d218609c61bb9fe94c91e5a2a3515364667028
IncinX <incinx@gmail.com>
2022-09-06 05:25:11 +0000

    Things are working a bit more, but the nextUntil doesn't return anything.

--- src/module/importers/farhome-rules.js
+++ src/module/importers/farhome-rules.js
@@ -6,13 +6,37 @@ export function parseRules(markdownString) {
 
   // Parse non-background feats first
   // #todo These aren't working, figure out how to get it to find the #basic tag
-  markdownHtmlQuery.find('a[href$="Basic"]').each((index, element) => {
-    console.log(element.innerHTML);
-  });
+  // #todo Add a check to only get #basic under a parent #feats tag
+  // #todo This is definitely not working... it should also be more resilient so that Basic will go until it finds the next header of the same level
+  const feats = markdownHtmlQuery
+    .find('a[href$="Basic"]')
+    //.nextUntil('a[href$="Journeyman"]')
+    .map((_index, element) => {
+      const level = 'Basic';
+      const name = element.innerHTML;
+      const description = element.nextSibling.innerHTML;
+
+      return { name, level, description };
+    })
+    .get();
+
+  console.log(feats);
 
   // #todo Add background feats as distinct set of feats
+  const backgrounds = [];
 
   // #todo Parse maneuvers
+  const maneuvers = [];
 
   // #todo Parse all spells for each school of magic separately
+  const spells = [];
+
+  const rulesData = {
+    feats,
+    backgrounds,
+    maneuvers,
+    spells,
+  };
+
+  return rulesData;
 }
--- src/module/importers/farhome-rules.test.js
+++ src/module/importers/farhome-rules.test.js
@@ -5,10 +5,19 @@ test('Parse Farhome Original Rules', async () => {
     'https://raw.githubusercontent.com/scrabbletank/farhome-rules/2aebacdcb0124c2cc5001a0d031e12eb5025cf2f/rulesdoc.md';
   const data = await fetch(rulesUri);
   const markdownText = await data.text();
-  parseRules(markdownText);
+  const rulesData = parseRules(markdownText);
 
-  // DEBUG! Just pass for now
-  expect(true).toBe(true);
+  expect(rulesData).toBeDefined();
+  expect(rulesData.feats).toBeDefined();
+  expect(rulesData.feats.length).toBeGreaterThan(0);
+  /*
+  expect(rulesData.backgrounds).toBeDefined();
+  expect(rulesData.backgrounds.length).toBeGreaterThan(0);
+  expect(rulesData.maneuvers).toBeDefined();
+  expect(rulesData.maneuvers.length).toBeGreaterThan(0);
+  expect(rulesData.spells).toBeDefined();
+  expect(rulesData.spells.length).toBeGreaterThan(0);
+  */
 });
 
 test('Parse Farhome Eberron Rules', async () => {
