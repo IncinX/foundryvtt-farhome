d269cc361878f31dbed9633fdc1a37b16c6c7a34
IncinX <incinx@gmail.com>
2022-09-25 13:02:34 -0700

    Lint fixes.

--- src/module/importers/5etools-monsters.js
+++ src/module/importers/5etools-monsters.js
@@ -1,18 +1,15 @@
-
 export function createCompendiumFrom5etoolsBeastiary(rulesUrl) {
   // #note This creates a world compendium. System compendiums are automatically created if they are defined in their system.json
 
   // #todo Need to check if the compendium exists already
 
-  CompendiumCollection.createCompendium(
-    {
-      name:'monster-compendium',
-      label:'Monster Compendium',
-      type:'Actor',
-      system:'farhome',
-      package:'system'
-    }
-  );
+  CompendiumCollection.createCompendium({
+    name: 'monster-compendium',
+    label: 'Monster Compendium',
+    type: 'Actor',
+    system: 'farhome',
+    package: 'system',
+  });
 
   // #todo If I can add a bunch of data to this on document creation, it will make it much more unit-testable if I have a parsing function that returns the data in a json layout, ready for document creation.
 
@@ -23,17 +20,17 @@ export function createCompendiumFrom5etoolsBeastiary(rulesUrl) {
         type: 'npc',
         data: {
           race: {
-            value: 'Human'
+            value: 'Human',
           },
           attributes: {
             str: { value: 3 },
-            dex: { value: 2 }
-          }
-        }
-      }
+            dex: { value: 2 },
+          },
+        },
+      },
     ],
     {
-      pack: 'world.monster-compendium'
-    }
+      pack: 'world.monster-compendium',
+    },
   );
 }
--- src/module/importers/farhome-rules.js
+++ src/module/importers/farhome-rules.js
@@ -1,24 +1,22 @@
 export function createCompendiumFromRules(rulesUrl) {
-  CompendiumCollection.createCompendium(
-    {
-      name:'feats-compendium',
-      label:'Feats Compendium',
-      type:'Item',
-      system:'farhome',
-      package:'system'
-    }
-  );
+  CompendiumCollection.createCompendium({
+    name: 'feats-compendium',
+    label: 'Feats Compendium',
+    type: 'Item',
+    system: 'farhome',
+    package: 'system',
+  });
 
   game.farhome.FarhomeItem.createDocuments(
     [
       {
         name: 'TestFeat',
-        type: 'feat'
-      }
+        type: 'feat',
+      },
     ],
     {
-      pack: 'world.feats-compendium'
-    }
+      pack: 'world.feats-compendium',
+    },
   );
 }
 
@@ -30,11 +28,14 @@ export function parseRules(markdownString) {
   // #todo These aren't working, figure out how to get it to find the #basic tag
   // #todo Add a check to only get #basic under a parent #feats tag
   // #todo This is definitely not working... it should also be more resilient so that Basic will go until it finds the next header of the same level
-  
+
   // DEBUG!
-  markdownHtmlQuery.filter('a[href$="Basic"]').siblings().each((i, el) => {
-    console.log(el.innerHTML);
-  });
+  markdownHtmlQuery
+    .filter('a[href$="Basic"]')
+    .siblings()
+    .each((i, el) => {
+      console.log(el.innerHTML);
+    });
 
   const feats = markdownHtmlQuery
     .find('a[href$="Basic"]')
@@ -43,7 +44,7 @@ export function parseRules(markdownString) {
     .map((_index, element) => {
       const level = 'Basic';
       const name = element.innerHTML;
-      const description = '';//element.nextSibling.innerHTML;
+      const description = ''; //element.nextSibling.innerHTML;
       console.log(name);
 
       return { name, level, description };
